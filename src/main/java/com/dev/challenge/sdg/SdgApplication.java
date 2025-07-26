5ef251ddb3a71800b25fb4e10fd2168aea5a7326
Naveen <preenav@Naveens-MacBook-Pro.local>
2025-07-26 13:31:11 -0700

    Initial commit - Spring Boot App

---
+++ src/main/java/com/dev/challenge/sdg/SdgApplication.java
@@ -0,0 +1,13 @@
+package com.dev.challenge.sdg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/challenge/sdg/SdgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.challenge.sdg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
